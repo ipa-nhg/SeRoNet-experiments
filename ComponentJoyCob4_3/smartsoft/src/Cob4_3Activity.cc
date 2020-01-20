ce471ed47d395b8df9118aaea6489caac8ce2568
ipa-nhg <nhg@ipa.fhg.de>
2020-01-20 17:14:30 +0100

    complete subscriber example

--- ComponentJoyCob4_3/smartsoft/src/Cob4_3Activity.cc
+++ ComponentJoyCob4_3/smartsoft/src/Cob4_3Activity.cc
@@ -39,16 +39,21 @@ void Cob4_3Activity::on_joyIn(const CommBasicObjects::CommJoystick &input)
 	//   there, use the method joyInGetUpdate(input) to get a copy of the input object
 	//std::unique_lock<std::mutex> lck (mtx);
 	//ComponentJoyCob4_3RosPortCallbacks comp;
+	if (input.getButtons()!=0 or input.get_x()!=0 or input.get_y()!=0 or input.get_y2()!=0){
+		sensor_msgs::Joy joy_msg;
+		joy_msg.axes.resize(6);
+		joy_msg.axes[0]=-input.get_x();
+		joy_msg.axes[1]=-input.get_y();
+		joy_msg.axes[3]=-input.get_x2();
+		joy_msg.axes[4]=input.get_y2();
 
-	sensor_msgs::Joy joy_msg;
-	joy_msg.buttons.resize(input.getButtons());
-	//for(size_t btn=0; btn < input.getButtons(); ++btn) {
-	//	joy_msg.buttons[btn]=input.get_button(btn);
-		// TODO: check if this conversion is correct
-	//}
+		joy_msg.buttons.resize(17);
+		for(size_t btn=0; btn < 8; ++btn) {
+			joy_msg.buttons[btn]=input.get_button(btn);
+		}
 
-	COMP -> rosPorts -> publish_ros_msgs(joy_msg);
-
+		COMP -> rosPorts -> publish_ros_msgs(joy_msg);
+	}
 
 }
 
@@ -70,12 +75,12 @@ int Cob4_3Activity::on_execute()
 	status = this->joyInGetUpdate(joyInObject);
 	if(status != Smart::SMART_OK) {
 		std::cerr << status << std::endl;
-		// return 0;
-	} else {
-		std::cout << "received: " << joyInObject << std::endl;
+		return 0;
+	//} else {
+	//	std::cout << "received: " << joyInObject << std::endl;
 	}
 
-	std::cout << "Hello from Cob4_3Activity " << std::endl;
+	//std::cout << "Hello from Cob4_3Activity " << std::endl;
 
 	// it is possible to return != 0 (e.g. when the task detects errors), then the outer loop breaks and the task stops
 	return 0;
